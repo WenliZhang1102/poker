46e56646758de17fcd322d1f3fd703e56edc3850
Aleš Kajzar <alesh.kajzar@gmail.com>
2013-06-20 22:27:42 +0200

    settings

--- PokerTimer/gen/com/example/pokertimer/R.java
+++ PokerTimer/gen/com/example/pokertimer/R.java
@@ -8,6 +8,10 @@
 package com.example.pokertimer;
 
 public final class R {
+    public static final class array {
+        public static final int backlight_time_list=0x7f080000;
+        public static final int backlight_time_list_values=0x7f080001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -33,33 +37,39 @@ public final class R {
         public static final int settings=0x7f020009;
     }
     public static final class id {
-        public static final int list=0x7f0a0000;
-        public static final int menu_add=0x7f0a0003;
-        public static final int menu_cancel=0x7f0a0002;
-        public static final int menu_ok=0x7f0a0001;
-        public static final int menu_search=0x7f0a0005;
-        public static final int menu_settings=0x7f0a0004;
+        public static final int list=0x7f0b0000;
+        public static final int menu_add=0x7f0b0003;
+        public static final int menu_cancel=0x7f0b0002;
+        public static final int menu_ok=0x7f0b0001;
+        public static final int menu_search=0x7f0b0005;
+        public static final int menu_settings=0x7f0b0004;
     }
     public static final class layout {
         public static final int games=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int add_game=0x7f090000;
-        public static final int games=0x7f090001;
+        public static final int add_game=0x7f0a0000;
+        public static final int games=0x7f0a0001;
     }
     public static final class raw {
         public static final int sql=0x7f050000;
     }
     public static final class string {
-        public static final int action_settings=0x7f070001;
-        public static final int add_game=0x7f070004;
+        public static final int add_game=0x7f070002;
         public static final int app_name=0x7f070000;
-        public static final int cancel=0x7f070007;
-        public static final int hello_world=0x7f070002;
-        public static final int ok=0x7f070006;
-        public static final int search=0x7f070003;
-        public static final int settings=0x7f070005;
+        public static final int cancel=0x7f070005;
+        public static final int ok=0x7f070004;
+        /**  MAIN MENU STRINGS 
+         */
+        public static final int search=0x7f070001;
+        public static final int settings=0x7f070003;
+        public static final int time_of_backlight=0x7f070009;
+        public static final int warn_1_minute=0x7f070007;
+        /**  SETTINGS STRINGS 
+         */
+        public static final int warn_before_round_ends=0x7f070006;
+        public static final int warn_on_round_ends=0x7f070008;
     }
     public static final class style {
         /** 
@@ -83,11 +93,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppTheme=0x7f090001;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
--- PokerTimer/src/com/example/pokertimer/SettingsActivity.java
+++ PokerTimer/src/com/example/pokertimer/SettingsActivity.java
@@ -1,23 +1,25 @@
 package com.example.pokertimer;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.widget.Toast;
+import android.preference.PreferenceFragment;
 
 public class SettingsActivity extends PreferenceActivity {
 	
-	 // @SuppressWarnings("deprecation")
 	@Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	       addPreferencesFromResource(R.xml.preferences);
-	        
-	       
-	        
-	        ActionBar ab = getActionBar();
-	        ab.setDisplayShowHomeEnabled(false);
-	        ab.setDisplayShowTitleEnabled(false);
-	    }
+    protected void onCreate(final Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
 
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
 }
