101972b10f712aeef0d7951d5e084e874eaed474
Ondřej Komárek <komoi@seznam.cz>
2013-07-07 23:47:26 +0200

    About text, landscape mode in countdown view

--- PokerTimer/src/com/example/pokertimer/AboutActivity.java
+++ PokerTimer/src/com/example/pokertimer/AboutActivity.java
@@ -3,8 +3,10 @@ package com.example.pokertimer;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 public class AboutActivity extends Activity {
 	
@@ -19,6 +21,18 @@ public class AboutActivity extends Activity {
 		ab.setDisplayHomeAsUpEnabled(true);
 
 	     
+	}
+	
+	 public void onClick(View v){
+		 if(v == findViewById(R.id.donate)){
+			 openDonateURL();
+		 }
+	 }
+	
+	public void openDonateURL() {
+	    Intent browse = new Intent( Intent.ACTION_VIEW , Uri.parse( "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=7LNF5KQAFKG3C&lc=CZ&item_name=Poker%20countdown%20timer%20app&currency_code=CZK&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted" ) );
+
+	    startActivity( browse );
 	}
 	
 	//When is back button in actionbar pressed
